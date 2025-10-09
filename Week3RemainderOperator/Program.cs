825a54f3f0b1ed088a9dbda5742a191c7b02a95c
Mattias Tamm <mattias.tamm@tktk.ee>
2025-10-10 00:09:07 +0300

    week 3

---
+++ Week3RemainderOperator/Program.cs
@@ -0,0 +1,28 @@
+﻿//Odd or even
+
+int a = 110;
+
+if result = a % 2;
+
+Console. WritleLine(result);
+
+//if (result == 0)
+//{
+//    Console.WriteLine($"{a} is an even number");
+//} else
+//{
+//    Console.WriteLine("Odd number");
+//}
+
+switch (result) 
+
+    case 0:
+        Console.WriteLine($"{a} is an even number");
+        break;
+    case 1:
+    Console.WriteLine($"{a} is an odd number");
+        break;  
+    default:
+        Console.WriteLine("Invalid value");
+        break;  
+
